8750af3f5fadd2bb8156e01b718b2947fa71d66a
Getahun Haftu <getahun19h@gmail.com>
2025-02-15 18:23:34 -0600

    js added & conducted

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,57 @@
+const configContainer = document.querySelector('.config-container');
+const quizContainer = document.querySelector('.quiz-container');
 const answerOptions = document.querySelector('.answer-options');
 const nextQuestionBtn = document.querySelector('.next-question-btn');
 const questionStatus = document.querySelector('.question-status');
+const timerDispaly = document.querySelector('.time-duration');
+const resultContainer = document.querySelector('.result-container');
 
+//quiz state variables
+const QUIZ_TIME_LIMIT=15;
+let currentTime = QUIZ_TIME_LIMIT;
+let timer=null;
 let quizCategory = "General Knowledge";
-let numberQuestions = 10;
+let numberQuestions = 5;
 let currentQuestion = null;
 const questioIndexHistory = [];
+let correctAnswerCount = 0;
+
+
+//display the quiz result and and hide the quiz container
+const showQuizResult = () =>{
+  quizContainer.style.display = "none";
+  resultContainer.style.display = "block";
+  const resultText=`You answered <b>${correctAnswerCount}</b> out of <b>${numberQuestions}</b> questions correctly. Great effert!`;
+  document.querySelector('.result-message').innerHTML =resultText;
+}
+// clear and reset timer
+const reseTimer=()=>{
+  clearInterval(timer);
+  currentTime=QUIZ_TIME_LIMIT;
+  timerDispaly.textContent=`${currentTime}s`;
+}
+//Initialize and start timer for the current qustion
+const startTimer=()=>{
+  timer=setInterval(()=>{
+    currentTime--;
+   timerDispaly.textContent=`${currentTime}s`;
+    if(currentTime<=0){
+      clearInterval(timer);
+      highlightCorrectAnswer();
+      nextQuestionBtn.style.visibility='visible';
+      quizContainer.querySelector('.quiz-timer').style.background="#c31402";
+      answerOptions.querySelectorAll(".answer-option").forEach(option => option.style.pointerEvents = 'none' );
+ 
+    }
+  },1000)
+}
+
 
 const getRandomQuestion = () =>{
  const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
-//the results if all questions have been used
+//show the results if all questions have been used
 if(questioIndexHistory.length >= Math.min(categoryQuestions.length,numberQuestions)){
-return console.log('Quiz Completed');
+return showQuizResult();
 }
 // filter out already asked questions and choose a random one
  const availaleQuestion = categoryQuestions.filter((_,index)=> !questioIndexHistory.includes(index));
@@ -30,9 +70,10 @@ const highlightCorrectAnswer = ()=>{
 }
 //handle the user's answer selection
 const handleAnswer = (option, answerIndex)=>{
+  clearInterval(timer);
   const isCorrect = currentQuestion.correctAnswer === answerIndex;
   option.classList.add(isCorrect ? 'correct' : 'incorrect');
-  !isCorrect ? highlightCorrectAnswer() :"";
+  !isCorrect ? highlightCorrectAnswer() :correctAnswerCount++;
   //insert icon based on the correctness
   const iconHTML = `<span class="material-symbols-rounded">${isCorrect ? 'check_circle': 'cancel'}</span>`
   option.insertAdjacentHTML('beforeend',iconHTML);
@@ -44,10 +85,14 @@ const handleAnswer = (option, answerIndex)=>{
 const renderQuestion = ()=>{
   currentQuestion  = getRandomQuestion();
    if(!currentQuestion) return;
+
+   reseTimer();
+   startTimer();
   //  console.log(currentQuestion); 
 //update the UI
 answerOptions.innerHTML ="";
 nextQuestionBtn.style.visibility='hidden';
+quizContainer.querySelector('.quiz-timer').style.background="#32313c";
  document.querySelector('.question-text').textContent = currentQuestion.question;
  questionStatus.innerHTML = `<b>${questioIndexHistory.length}</b> of <b>${numberQuestions}</b> Question`;
  //create option <li> elements and append them, and add click envent listner
@@ -59,6 +104,34 @@ nextQuestionBtn.style.visibility='hidden';
   li.addEventListener('click',()=>handleAnswer(li, index));
  });
 }
+//start the quiz and render the random question
+const startQuiz = ()=>{
+  configContainer.style.display ='none';
+  quizContainer.style.display ="block";
 
+// update the quiz category and  number of questions
+  quizCategory = configContainer.querySelector('.category-option.active').textContent;
+  numberQuestions = parseInt(configContainer.querySelector('.question-option.active').textContent);
+  
+
+  renderQuestion();
+}
+//highlight the selected option on the click -category or no. of questions
+document.querySelectorAll(".category-option, .question-option").forEach(option => {
+  option.addEventListener('click',()=>{
+    option.parentNode.querySelector('.active').classList.remove("active");
+    option.classList.add('active');
+  });
+});
+//reset the quiz and return to the configuration container
+const resetQuiz=()=>{
+  reseTimer();
+  correctAnswerCount=0;
+  questioIndexHistory.length=0;
+  configContainer.style.display='block';
+  resultContainer.style.display='none';
+}
 renderQuestion();
 nextQuestionBtn.addEventListener('click', renderQuestion);
+document.querySelector(".try-again-btn").addEventListener('click',resetQuiz);
+document.querySelector(".start-quiz-btn").addEventListener('click',startQuiz)
